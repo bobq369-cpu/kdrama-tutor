d677e2e65b18c5891d2b94e938a7d5ccdf1d0167
AndrewJung <bobq369@gmail.com>
2026-02-01 02:56:58 +0700

    Remove top padding completely and adjust h1 margin

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def inject_custom_css():
     title_y = "-200px"    # 상하 이동 (음수: 위로, 양수: 아래로)
 
     # [4] 추천 표현 바 위치
-    adjust_smart_y = "150px"
+    adjust_smart_y = "100px"
     adjust_smart_x = "0px"
 
     # [5] 역할 캡션("💡 역할: ...") 위치 (유체이탈)
