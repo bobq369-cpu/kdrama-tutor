0dbe66a70be2be44699aa047a79e38eef76daed1
AndrewJung <bobq369@gmail.com>
2026-02-01 08:07:27 +0700

    .

--- app.py
+++ app.py
@@ -52,8 +52,8 @@ def inject_custom_css():
     REMOCON["title_left_offset"] = title_left_offset
 
     # [4] 역할 설명 — 절대 위치
-    role_top = "100px"
-    role_left_offset = "100px"
+    role_top = "150px"
+    role_left_offset = "200px"
     REMOCON["role_top"] = role_top
     REMOCON["role_left_offset"] = role_left_offset
 
